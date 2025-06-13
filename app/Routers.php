5a9680549a68e5e8f57409ab567eadad73110487
Dvid Nguyen <=qndjxz@gmail.com>
2025-06-13 09:58:27 +0700

    add header and navbar

--- app/Routers.php
+++ app/Routers.php
@@ -2,10 +2,13 @@
 
 // use app\core\Controller;
 
-
 Router::get('/', 'HomeController@show', [
-    'AuthenMiddleware@show',
-    'AuthenMiddleware@show2'
+    // 'AuthenMiddleware@show',
+    // 'AuthenMiddleware@show2'
+]);
+Router::get('/listroom', 'ListRoomController@show', [
+    // 'AuthenMiddleware@show',
+    // 'AuthenMiddleware@show2'
 ]);
 
 Router::get('/news/{id}', 'NewsController@show', [
