c3650657f45a00b6b7a282867c083b591d93e4db
HuynhHa17 <huynhdaihafc@gmail.com>
2025-06-15 21:59:57 +0700

    detail

--- app/controllers/DetailRoomController.php
+++ app/controllers/DetailRoomController.php
@@ -7,18 +7,32 @@ use app\models\DetailRoomModel;
 
 class DetailRoomController extends Controller
 {
-    function __construct()
+    private DetailRoomModel $model;
+
+    public function __construct()
     {
         parent::__construct();
+        $this->model = new DetailRoomModel();
     }
 
-    function show($req, $res)
+    public function show($req, $res)
     {
-        $this->render('show');
+        $id = $req->get('id'); 
+        $rooms = $this->model->getAllRooms();
+        $roomDetail = null;
+
+        if ($id && is_numeric($id)) {
+            $roomDetail = $this->model->getRoomById((int)$id);
+        }
+
+        return $res->view('show', [
+            'rooms' => $rooms,
+            'roomDetail' => $roomDetail
+        ]);
     }
 
-    function index($req, $res)
+    public function index($req, $res)
     {
-        return $res->error('user isnt exits')->send();
+        return $res->error('User does not exist')->send();
     }
 }
