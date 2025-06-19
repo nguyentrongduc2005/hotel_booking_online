f10ff912f6b6b8e9b658a11d3ae8a1f7061cb508
dvidnguyen <qndjxz@gmail.com>
2025-06-19 13:49:31 +0700

    fix login and register

--- app/views/auth/regis.php
+++ app/views/auth/regis.php
@@ -1,20 +1,23 @@
-
 <link rel="stylesheet" href="<?= $this->configs->config['pathAssets'] ?>css/regis.css?v=<?= time() ?>">
+
 
 <div class="regis-bg-overlay">
   <div class="regis-main-container">
     <div class="regis-right" style="background-image: url('<?= $this->configs->config['pathAssets'] ?>/img/bg-regis.png')"></div>
     <div class="regis-left">
       <div class="regis-title">CREATE YOUR ACCOUNT</div>
-      <form class="regis-form" method="post" action="">
-        <input type="text" name="fullname" placeholder="Full Name" required>
+      <form class="regis-form" method="post" action="<?= $this->configs->config['basePath'] ?>regis">
+        <input type="text" name="full_name" placeholder="Full Name" required>
         <input type="email" name="email" placeholder="Email" required>
         <div class="regis-row-flex">
-          <input type="text" name="phone" placeholder="Phone Number" required>
-          <input type="text" name="id" placeholder="ID" required>
+          <input type="text" name="sdt" placeholder="Phone Number" required>
+          <input type="text" name="cccd" placeholder="ID" required>
         </div>
-        <input type="password" name="password" placeholder="Password" required>
+        <input type="password" name="pass" placeholder="Password" required>
         <button type="submit" class="regis-submit-btn">Create account</button>
+        <?php if (!empty($message)): ?>
+          <div class="regis-message"><?= htmlspecialchars($message) ?></div>
+        <?php endif; ?>
       </form>
       <div class="regis-bottom-link">
         Already you have an account?
