4a1721daf8b9d9290453c3890e1c6a06d41f65e7
Duc <nguyentrongduc447@gmail.com>
2025-06-24 14:58:41 +0700

    add payment model and controller

--- app/controllers/PaymentController.php
+++ app/controllers/PaymentController.php
@@ -3,11 +3,20 @@
 namespace app\controllers;
 
 use app\core\Controller;
+use app\models\PaymentModel;
 
 class PaymentController extends Controller
 {
+
+    private  $model;
+    function __construct()
+    {
+        parent::__construct();
+        $this->model = new PaymentModel();
+    }
+
     public function show($req, $res)
     {
         $this->render('index', []);
     }
-} 
+}
