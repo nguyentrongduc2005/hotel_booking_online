9b95a1b5b708613fa467f8d4008a7022f24f2c6e
Duc <nguyentrongduc447@gmail.com>
2025-06-26 13:07:46 +0700

    add verify token cho admin and cancelled

--- app/views/AdminLayouts/header.php
+++ app/views/AdminLayouts/header.php
@@ -1,7 +1,21 @@
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="<?= $this->configs->config['pathAssets'] ?>css/adminHeader.css?v=<?= time() ?>">
 <header class="admin-header">
-  <div class="admin-header-right">
-    <i class="fa fa-user-circle" style="font-size:2rem;color:black;"></i>
-  </div>
+    <div class="admin-header-right">
+        <i class="fa fa-user-circle" style="font-size:2rem;color:black;"></i>
+    </div>
 </header>
+<script src="<?= $this->configs->config['pathAssets'] ?>js/dashboard.js?v=<?= time() ?>"></script>
+<?php if (isset($_SESSION["timer"])) {
+
+  $path  = $this->configs->config['basePath'];
+  $leftTime = $_SESSION["timer"] - time();
+
+  $leftTime = max(0, ($leftTime - 120) * 1000);
+  if ($leftTime > 0) {
+    echo " <script>
+   setTimeout(function() { 
+   checktokenTimer('{$path}')
+                            }, " . $leftTime . ")</script>";
+  }
+} ?>
