111a3eded3fb977cb910b19fbdb4b122cd4b6812
dvidnguyen <qndjxz@gmail.com>
2025-06-28 17:10:55 +0700

    fix ui

--- app/views/error/index.php
+++ app/views/error/index.php
@@ -4,70 +4,42 @@
 <head>
     <meta charset="UTF-8">
     <title>Error</title>
-    <style>
-        body {
-            background-color: #f8d7da;
-            color: #721c24;
-            font-family: Arial, sans-serif;
-            padding: 40px;
-            align-items: center;
-        }
-
-        .error-box {
-
-            background-image: url('<?= $this->configs->config['pathAssets'] ?>/img/error/boxx.gif');
-            background-size: contain;
-            background-repeat: no-repeat;
-            padding: 30px;
-            border-radius: 10px;
-            border: 1px solid #f1aeb5;
-            display: inline-block;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-            text-align: center;
-            width: 400px;
-            margin: 0 auto;
-            height: 400px;
-        }
-
-        h1 {
-            font-size: 2em;
-            margin-bottom: 15px;
-        }
-
-        .countdown {
-            font-weight: bold;
-            color: #721c24;
-            font-size: 1.2em;
-        }
-
-        button {
-            background-color: #721c24;
-            color: #fff;
-            padding: 10px 20px;
-            border: none;
-            margin-top: 20px;
-            cursor: pointer;
-            border-radius: 5px;
-        }
-
-        button:hover {
-            background-color: #501217;
-        }
-    </style>
+    <link rel="stylesheet" href="<?= $this->configs->config['pathAssets'] ?>css/error.css?v=<?= time() ?>" />
 </head>
 
-<body <?php
-        if ($data['message']) {
-            $message = $data['message'];
-        } else {
-            $message = "Đã xảy ra lỗi không xác định. Vui lòng thử lại sau.";
-        }
-        ?>
-    <div class="error-box">
-    <h1>⚠️ An Error Occurred</h1>
-    <p><?= $message ?></p>
-    <p>Trang sẽ chuyển sau <span class="countdown"><?= $timeout ?></span>seconds...</p>
-    <button onclick="redirectNow()">Back</button>
+<body>
+    <?php
+    if ($data['message']) {
+        $message = $data['message'];
+    } else {
+        $message = "Đã xảy ra lỗi không xác định. Vui lòng thử lại sau.";
+    }
+    ?>
+    <div class="error-container">
+        <h1>⚠️ An Error Occurred</h1>
+        <div class="animation-container">
+            <div aria-label="Orange and tan hamster running in a metal wheel" role="img" class="wheel-and-hamster">
+                <div class="wheel"></div>
+                <div class="hamster">
+                    <div class="hamster__body">
+                        <div class="hamster__head">
+                            <div class="hamster__ear"></div>
+                            <div class="hamster__eye"></div>
+                            <div class="hamster__nose"></div>
+                        </div>
+                        <div class="hamster__limb hamster__limb--fr"></div>
+                        <div class="hamster__limb hamster__limb--fl"></div>
+                        <div class="hamster__limb hamster__limb--br"></div>
+                        <div class="hamster__limb hamster__limb--bl"></div>
+                        <div class="hamster__tail"></div>
+                    </div>
+                </div>
+                <div class="spoke"></div>
+            </div>
+        </div>
+        <p><?= $message ?></p>
+        <p>Trang sẽ chuyển sau <span class="countdown"><?= $timeout ?></span> seconds...</p>
+        <button onclick="redirectNow()">Back</button>
     </div>
 
     <script>
@@ -88,6 +60,7 @@
             window.location.href = redirectUrl;
         }
     </script>
+
 
 </body>
 
