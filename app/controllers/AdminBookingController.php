f8255b7037226b37adc2819da0466aa8c9c877db
HuynhHa17 <huynhdaihafc@gmail.com>
2025-07-07 01:15:12 +0700

    update

--- app/controllers/AdminBookingController.php
+++ app/controllers/AdminBookingController.php
@@ -11,7 +11,6 @@ class AdminBookingController extends Controller
     private $model;
 
     public function __construct()
-    public function __construct()
     {
         parent::__construct();
         $this->model = new AdminBookingModel();
